aec919906785df0758d38bb077980bc5e361cc62
jiwonshin84 <jiwonshin84@gmail.com>
2026-05-06 12:03:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -540,7 +540,7 @@ elif menu == "Gemini 챗봇":
                 try:
 
                     model = genai.GenerativeModel(
-                        "gemini-1.5-flash"
+                        "gemini-2.0-flash"
                     )
 
                     prompt = f"""
